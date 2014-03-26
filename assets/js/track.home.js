d49b1730dca0e074bac793a684ac88a67553674e
Jeff <jeff@webgoal.com.br>
2014-03-26 18:17:35 -0300

    Teste do mixpanel.

--- assets/js/track.home.js
+++ assets/js/track.home.js
@@ -5,52 +5,51 @@ var focusOnEmail = false
 var keyboardPressedForEmail = false
 
 $(function() {
-	mixpanel.track("access_page", { "page": "home" })
+	mixpanel.track("Acesso a Home")
+	mixpanel.set_config({ track_links_timeout:10000 })
+
+	mixpanel.track_links(".como_funciona", "Link para Como Funciona")
+	mixpanel.track_links(".funcionalidades", "Link para Funcionalidades")
+	mixpanel.track_links(".cadastre_se", "Link para o Formuário")
+	mixpanel.track_forms("#cadastro", "Envio do formulário")
 
 	$('#btn-one').on('click', function() {
-		mixpanel.track("click", { "botao": "topo" })
+		mixpanel.track("CallToAction do topo")
+	})
+	$('#btn-two').click(function() {
+		mixpanel.track("CallToAction do meio")
 	})
 
 	$('#nome').on('focus', function() {
 		if (!focusOnNome) {
-			mixpanel.track("focus", { "nome": true })
+			mixpanel.track("Foco no nome")
 			focusOnNome = true
 		}
 	})
 	$('#nome').on('keydown', function() {
 		if (!keyboardPressedForNome) {
-			mixpanel.track("keydown", { "nome": true })
+			mixpanel.track("Escreveu o nome")
 			keyboardPressedForNome = true
 		}
 	})
 
 	$('#email').on('focus', function() {
 		if (!focusOnEmail) {
-			mixpanel.track("focus", { "email": true })
+			mixpanel.track("Foco no Email")
 			focusOnEmail = true
 		}
 	})
 	$('#email').on('keydown', function() {
 		if (!keyboardPressedForEmail) {
-			mixpanel.track("keydown", { "email": true })
+			mixpanel.track("Escreveu o email")
 			keyboardPressedForEmail = true
 		}
 	})
 
-	$('#btn-two').click(function() {
-		mixpanel.track("click", { "botao": "meio" })
-	})
-	$('#btn-submit').on('click', function() {
-		mixpanel.track("submit", {
-			"form": "cadastro"
-		}, function() {
-			$('form').submit()
-		})
-	})
 	$(window).scroll(function() {
 		if($(window).scrollTop() + $(window).height() == $(document).height()) {
 			if (!scrolledToBottom) {
-				mixpanel.track("view", { "form": "cadastro" })
+				mixpanel.track("Visualizou o formulário")
 				scrolledToBottom = true
 			}
 		}
